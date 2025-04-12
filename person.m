d79231c28504e172f31962b8337670bce46f2dd9
Miku <miku@mikucloud.co>
2025-04-12 23:58:54 +0800

    Fixed the Chinese garbled characters caused by GBK issues

--- person.m
+++ person.m
@@ -1,4 +1,4 @@
-classdef person  %琛屼汉绫�
+classdef person  %行人类
     %person are the object which run on roads
     
     properties
@@ -95,10 +95,10 @@ classdef person  %琛屼汉绫�
             A = zeros(2,1);
             U = zeros(2,1);
             
-            %鑷┍鍔�
+             %自驱力
             Fa(:,1) = [obj.m * (obj.Va_x - obj.U(1,t_r))/obj.Ta; obj.m*(obj.Va_y - obj.U(2,t_r))/obj.Ta];
             
-            %�?撹矾浣跨敤鑰呯殑浣滅敤鍔�
+            %道路使用者的作用力
             Fb(:,1) = 0;
 
             for i = 1:length(argu1)
@@ -127,33 +127,33 @@ classdef person  %琛屼汉绫�
                 end
             end
             
-            %�?撹矾杈圭晫鐨勪綔鐢ㄥ姏 
+            %道路边界的作用力 
             Fc(:,1) = [0;obj.Ac * exp((obj.r - abs(obj.Profile(2,t_r) - (20-0.5)))/obj.Bo) - obj.Ac * exp((obj.r-abs(obj.Profile(2,t_r) - (22+0.5)))/obj.Bo)];
             
-            %鍔涘拰鍔涚煩鐨勮緭鍏�
+            %力和力矩的输入
 
             ktheta = obj.I * obj.kl * Fa(2,1);
             komega=obj.I*(1+obj.alpha)*sqrt(obj.kl*abs(Fa(2,1))/obj.alpha);
-            Fu1 = obj.ko*(Fa(2,1) + Fb(2,1) + Fc(2,1)) - obj.kd * obj.U(2,t_r);        %娉曞?戝姏
+            Fu1 = obj.ko*(Fa(2,1) + Fb(2,1) + Fc(2,1)) - obj.kd * obj.U(2,t_r);        %法向力
             if (t_r>1)
-                theta_i = atan2(obj.U(1,t_r),obj.U(2,t_r));                 %鑸?戣1
-                theta_i_old = atan2(obj.U(1,t_r_old),obj.U(2,t_r_old));     %鑸?戣2
+                theta_i = atan2(obj.U(1,t_r),obj.U(2,t_r));                 %航向角1
+                theta_i_old = atan2(obj.U(1,t_r_old),obj.U(2,t_r_old));     %航向角2
             else
                 theta_i = atan(obj.U(1,t_r)/obj.U(2,t_r));
                 theta_i_old = 0;
             end
 
-            omega_i = (theta_i-theta_i_old)/obj.q;                   %瑙掗€熷害
-            u_theta = -ktheta*(theta_i-0)-komega*omega_i;            %鍔涚煩
-            Fu2 =-(u_theta/obj.I)*cos(theta_i);                      %鍔涚煩杞负娉曞?戝姏鐨勫舰寮?
+            omega_i = (theta_i-theta_i_old)/obj.q;                   %角速度
+            u_theta = -ktheta*(theta_i-0)-komega*omega_i;            %力矩
+            Fu2 =-(u_theta/obj.I)*cos(theta_i);                      %力矩转为法向力的形式
             
-            %璁＄畻�?堝姏
+            %计算合力
             F(:,1) = [Fa(1,1)+Fb(1,1)+Fc(1,1); Fu1 + Fu2];
 
-            %璁＄畻鍔犻€熷害
+            %计算加速度
             A(:,1) = [F(1,1)/obj.m; F(2,1)/obj.m];
 
-             %璁＄畻閫熷害
+             %计算速度
              U(2,1) =  obj.U(2,t_r) + A(2,1) * obj.q;
              U(1,1) = obj.U(1,t_r) + A(1,1) * obj.q;
             %if obj.U(1,t_r) + A(1,1) * obj.q >= 0
